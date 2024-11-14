78b66ca1ea2297311121d9c34649d5ddab12d497
noldor48 <noldor48stic@gmail.com>
2024-11-14 09:44:49 +0200

    pp
    pp

--- j11124.py
+++ j11124.py
@@ -1,20 +1,31 @@
 import json
 
-with open('Personas.json', 'r') as openfile:
-    json_object = json.load(openfile)           
-print(json_object)
-class Person:
-    def __init__(self, firstname, lastname, birthday, age, gender, email):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.birthday = birthday
-        self.age = age
-        self.gender = gender
-        self.email = email
-    def print_info(self):
-        print(self.firstname,self.lastname)
-    def find_perason(self):
-        print()
+personasss = []
+def pievienot_cilveku():
+    firstname = input("Ievadiet vardu: ")
+    lastname = input("Ievadiet uzvardu: ")
+    birthday = input("Ievadiet birthday: ")
+    age = input("Ievadiet vecumu: ")
+    gender = input("Ievadiet dzimumu: ")
+    email = input("Ievadiet email: ")
+    dictionary = {"firstname":firstname,
+                    "lastname":lastname,
+                    "birthday":birthday,
+                    "age":age,
+                    "gender":gender,
+                    "email":email}
+    file = open('Personas.json', 'r')
+    data=json.load(file)
+    file.close()
+    json_object = json.dumps(dictionary, indent=4)
+    with open("Personas.json", "w", encoding="utf-8") as outfile:
+        outfile.write(json_object)
+    print("Dati ierakstiti faila Personas.json.")
+
+def paradit_cilvekus():
+    with open('Personas.json', 'r') as openfile:
+        json_object = json.load(openfile)           
+    print(json_object)
 
 class main():
     while True:
@@ -22,53 +33,29 @@ class main():
             print("2. Paradit visus cilvekus")
             print("3. Meklet cilveku")
             print("4. Iziet")
-
             choice = input("Izveleties darbibu: ")
 
             if choice == '1':
-                firstname = input("Ievadiet vardu: ")
-                lastname = input("Ievadiet uzvardu: ")
-                birthday = input("Ievadiet birthday: ")
-                age = input("Ievadiet vecumu: ")
-                gender = input("Ievadiet dzimumu: ")
-                email = input("Ievadiet email: ")
-                dictionary = {"firstname":firstname,
-                              "lastname":lastname,
-                              "birthday":birthday,
-                              "age":age,
-                              "gender":gender,
-                              "email":email}
-                with open('Personas.json', 'r') as openfile:
-                    json_object = json.load(openfile)           
-                print(json_object)
-                dictator = dictionary
-                dictator.update(json_object)
-                json_object = json.dumps(dictionary, indent=4)
-                with open("Personas.json", "w", encoding="utf-8") as outfile:
-                    outfile.write(json_object)
-                print("Dati ierakstiti faila Personas.json.")
-
-
+                pievienot_cilveku()
             elif choice == '2':
-                pass
-
+                paradit_cilvekus()
             elif choice == '3':
-                print()
+                paradit_cilvekus()
             elif choice == '4':
                 print("Iziet no programas.")
                 break
-
             else:
                 print("Nepareiza izvēle, mēģiniet vēlreiz")
 
-
 main()
-'''
-def add(args):
-    with open(DATA_FILENAME, mode='r', encoding='utf-8') as feedsjson:
-        feeds = json.load(feedsjson)
-    with open(DATA_FILENAME, mode='w', encoding='utf-8') as feedsjson:
-        entry = {}
-        entry['name'] = args.name
-        entry['url'] = args.url
-        json.dump(entry, feedsjson)'''
+
+
+
+
+
+
+'''with open('Personas.json', 'r') as openfile:
+        json_object = json.load(openfile)           
+    print(json_object)
+    dictator = dictionary
+    dictator.update(json_object)'''
